043e82c03c5f7d8cc06dab73318c0b378c909842
jayeshkadam785 <jayeshkadam785@gmail.com>
2026-09-06 00:48:33 +0530

    Add static_url_path parameter to Flask app initialization

--- api/index.py
+++ api/index.py
@@ -26,6 +26,7 @@ app = Flask(
     __name__,
     template_folder="../templates",
     static_folder="../static",
+    static_url_path="/static",
 )
 
 app.secret_key = os.environ.get("SECRET_KEY", "pashurakshak-dev-secret-change-me")
